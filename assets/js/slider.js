25292b1d3c3f123907cc0780d2fb366829e5d53c
silverskg <chokichoki_gm@i.softbank.jp>
2023-06-01 18:19:06 +0900

    アニメーション追加

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -7,11 +7,43 @@ imageUrls.forEach(function (url) {
 slider.slick({
   dots: true,
   infinite: true,
-  speed: 300,
+  speed: 1000,
   slidesToShow: 1,
   adaptiveHeight: true,
   autoplay: true,
   autoplaySpeed: 3000,
   fade: true,
-  cssEase: "linear",
+  cssEase: "ease-in-out",
 });
+
+// slickスライドの各スライドの変更時にズームクラスを削除してリセットする
+slider.on('beforeChange', function(event, slick, currentSlide, nextSlide){
+  $('.slick-slide').removeClass('zoom');
+});
+
+// スライドが切り替わった後にズームクラスを追加する
+slider.on('afterChange', function(event, slick, currentSlide){
+  $('.slick-slide').eq(currentSlide).addClass('zoom');
+});
+
+
+
+let options = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.1
+}
+
+let observer = new IntersectionObserver(onIntersect, options);
+let targets = document.querySelectorAll('.slick-slide');
+targets.forEach(target => observer.observe(target));
+
+function onIntersect(entries, observer) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('zoom');
+    } else {
+      entry.target.classList.remove('zoom');
+    }
+  });
+}
